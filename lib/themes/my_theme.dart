6d228d379f41dbfce2296c33b04a9a314cda4ec4
saci zakaria <sacideveloppeur@gmail.com>
2023-11-19 14:43:02 +0100

    ThemeData

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -6,7 +6,33 @@ class MyTheme {
     required BuildContext context,
   }) {
     return isDarkTheme
-        ? ThemeData() // DarkTheme
-        : ThemeData();// LightThem
+        ? ThemeData(
+            scaffoldBackgroundColor: const Color(0xFF343541),
+            primarySwatch: Colors.purple,
+            primaryColorDark: Colors.deepPurple,
+            dividerColor: Colors.white,
+            disabledColor: Colors.grey,
+            cardColor: const Color(0xFF444654),
+            canvasColor: Colors.black,
+            brightness: Brightness.dark,
+            appBarTheme: const AppBarTheme(color: Color(0xFF444654)),
+            buttonTheme: Theme.of(context)
+                .buttonTheme
+                .copyWith(colorScheme: const ColorScheme.dark()),
+          ) // DarkTheme
+        : ThemeData(
+            scaffoldBackgroundColor: Colors.grey.shade300,
+            primarySwatch: Colors.purple,
+            primaryColorDark: Colors.deepPurple,
+            dividerColor: Colors.black,
+            disabledColor: Colors.grey,
+            cardColor: Colors.white,
+            canvasColor: Colors.grey[50],
+            brightness: Brightness.light,
+            appBarTheme: const AppBarTheme(color: Colors.white),
+            buttonTheme: Theme.of(context)
+                .buttonTheme
+                .copyWith(colorScheme: const ColorScheme.light()),
+          ); // LightThem
   }
 }
