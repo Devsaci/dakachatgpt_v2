7de20a31bf302041f204a8e4571d11c6a1748fd9
saci zakaria <sacideveloppeur@gmail.com>
2023-11-19 14:50:05 +0100

    set setTheme(bool value)

--- lib/providers/my_theme_provider.dart
+++ lib/providers/my_theme_provider.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
-class MyThemeProvider extends ChangeNotifier {}
+class MyThemeProvider extends ChangeNotifier {
+  bool _darkTheme = false;
+
+  bool get themeType => _darkTheme;
+
+  set setTheme(bool value) {
+    _darkTheme = value;
+    notifyListeners();
+  }
+}
