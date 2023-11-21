3cedb26df00cae445417e388b9bad4eaab363526
saci zakaria <sacideveloppeur@gmail.com>
2023-11-21 10:18:28 +0100

    CircleAvatar(),

--- lib/authentication/registration_screen.dart
+++ lib/authentication/registration_screen.dart
@@ -10,6 +10,16 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              CircleAvatar(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
