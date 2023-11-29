5933fa626be1eda8970730e83cdee6ecfefd89d3
saci zakaria <sacideveloppeur@gmail.com>
2023-11-29 10:08:41 +0100

    bool _isSignedIn = false;

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AuthenticationProvider extends ChangeNotifier {
   bool _isLoading = false;
   bool _isSuccessful = false;
+  bool _isSignedIn = false;
 }
