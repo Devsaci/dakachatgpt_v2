4f421bf331f58d903efb9867e0bdf15464616c06
saci zakaria <sacideveloppeur@gmail.com>
2023-11-29 10:23:05 +0100

    Format Code

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -3,21 +3,21 @@ import 'package:flutter/material.dart';
 class AuthenticationProvider extends ChangeNotifier {
   bool _isLoading = false;
 
+  bool _isSuccessful = false;
+
+  bool _isSignedIn = false;
+
+  String? _uid;
+
   bool get isLoading => this._isLoading;
   set isLoading(bool value) => this._isLoading = value;
 
-  bool _isSuccessful = false;
   bool get isSuccessful => this._isSuccessful;
-
   set isSuccessful(bool value) => this._isSuccessful = value;
 
-  bool _isSignedIn = false;
   bool get isSignedIn => this._isSignedIn;
-
   set isSignedIn(bool value) => this._isSignedIn = value;
 
-  String? _uid;
   String? get uid => this._uid;
-
   set uid(String? value) => this._uid = value;
 }
