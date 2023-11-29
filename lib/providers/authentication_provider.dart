a7a990cb6532eba268afdb23760358211f1e3d15
saci zakaria <sacideveloppeur@gmail.com>
2023-11-29 10:07:46 +0100

    bool _isLoading = false;

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
-class AuthenticationProvider extends ChangeNotifier {}
+class AuthenticationProvider extends ChangeNotifier {
+  bool _isLoading = false;
+}
