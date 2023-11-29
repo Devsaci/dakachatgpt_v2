0928696b6bda33ba20fb55803078d5598f85c1cc
saci zakaria <sacideveloppeur@gmail.com>
2023-11-29 10:18:12 +0100

    get/set  _isLoading

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 class AuthenticationProvider extends ChangeNotifier {
   bool _isLoading = false;
+
+  bool get isLoading => this._isLoading;
+  set isLoading(bool value) => this._isLoading = value;
+
   bool _isSuccessful = false;
   bool _isSignedIn = false;
+  String? _uid;
 }
