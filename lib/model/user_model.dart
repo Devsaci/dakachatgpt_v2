5f9e623e49b81202cb81bc1fb2da6f62d6d8e013
saci zakaria <sacideveloppeur@gmail.com>
2023-12-07 09:21:43 +0100

    class UserModel

---
+++ lib/model/user_model.dart
@@ -0,0 +1,21 @@
+class UserModel {
+  String uid;
+  String name;
+  String profilePic;
+  String phone;
+  String aboutMe;
+  String lastSeen;
+  String dateJoined;
+  bool isOnline;
+  UserModel({
+    required this.uid,
+    required this.name,
+    required this.profilePic,
+    required this.phone,
+    required this.aboutMe,
+    required this.lastSeen,
+    required this.dateJoined,
+    required this.isOnline,
+  });
+  //
+}
