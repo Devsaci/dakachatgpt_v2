9526f507255a403bce1dd5f5f04d2db95ec43586
saci zakaria <sacideveloppeur@gmail.com>
2023-12-07 10:18:32 +0100

    userModel.lastSeen

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -126,6 +126,7 @@ class AuthenticationProvider extends ChangeNotifier {
       String imageUrl = await storeFileImageToStorage(
           '${Constants.userImages}/$uid.jpg', fileImage);
       userModel.profilePic = imageUrl;
+      userModel.lastSeen = DateTime.now().microsecondsSinceEpoch.toString();
     } on FirebaseException catch (e) {
       _isLoading = false;
       notifyListeners();
