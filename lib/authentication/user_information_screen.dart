c836e71c0e992a621e94b31b35759cb1e525a177
saci zakaria <sacideveloppeur@gmail.com>
2023-12-07 10:41:55 +0100

    if(nameController.text.length >= 3)

--- lib/authentication/user_information_screen.dart
+++ lib/authentication/user_information_screen.dart
@@ -306,6 +306,7 @@ class _UserInformationScreenState extends State<UserInformationScreen> {
       isOnline: true,
     );
     if (finalImageFile != null) {
+      if (nameController.text.length >= 3) {}
     } else {
       btnController.reset();
       showSnackBar(
