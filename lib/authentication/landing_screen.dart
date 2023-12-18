6d694a6f8ef0d967a454faacb971e98d94158612
saci zakaria <sacideveloppeur@gmail.com>
2023-12-18 11:31:30 +0100

    Scaffold  CircularProgressIndicator

--- lib/authentication/landing_screen.dart
+++ lib/authentication/landing_screen.dart
@@ -10,6 +10,10 @@ class LandingScreen extends StatefulWidget {
 class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: CircularProgressIndicator(
+        color: Colors.orangeAccent,
+      ),
+    );
   }
 }
