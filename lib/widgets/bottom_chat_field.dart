2b2f2256736c6b28dc555782ded7439fb2a35345
saci zakaria <sacideveloppeur@gmail.com>
2023-12-19 14:44:35 +0100

    class BottomChatField extends StatefulWidget

---
+++ lib/widgets/bottom_chat_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomChatField extends StatefulWidget {
+  const BottomChatField({super.key});
+
+  @override
+  State<BottomChatField> createState() => _BottomChatFieldState();
+}
+
+class _BottomChatFieldState extends State<BottomChatField> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
