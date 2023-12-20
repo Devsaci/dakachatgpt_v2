afa7a809f2864905725a8b9cc9871fe604fa04c0
saci zakaria <sacideveloppeur@gmail.com>
2023-12-20 10:10:12 +0100

    bool get isText

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -5,5 +5,9 @@ class ChatProvider extends ChangeNotifier {
   bool get isTyping => this._isTyping;
 
   set isTyping(bool value) => this._isTyping = value;
+
   bool _isText = true;
+  bool get isText => this._isText;
+
+  set isText(bool value) => this._isText = value;
 }
