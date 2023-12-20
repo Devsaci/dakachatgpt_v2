e120008a588999ade4ebd1966061f0052d6112ca
saci zakaria <sacideveloppeur@gmail.com>
2023-12-20 10:09:41 +0100

    bool get isTyping

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
   bool _isTyping = false;
+  bool get isTyping => this._isTyping;
+
+  set isTyping(bool value) => this._isTyping = value;
   bool _isText = true;
 }
