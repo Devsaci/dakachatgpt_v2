93524751bb3334ff05b456f2bfb94d59cf994849
saci zakaria <sacideveloppeur@gmail.com>
2023-12-28 18:46:36 +0100

    if (isText)

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,8 +1,26 @@
 class ApiService {
   // send message and get answers
-  sendMessageToChatGPT({
+  static Future<String> sendMessageToChatGPT({
     required String message,
     required String modelId,
     required bool isText,
-  }) {}
+  }) async {
+    if (isText) {
+      // generate a text response
+      try {
+        String answer = '';
+        return answer;
+      } catch (e) {
+        rethrow;
+      }
+    } else {
+      // generate an image response
+      try {
+        String imageUrl = '';
+        return imageUrl;
+      } catch (e) {
+        rethrow;
+      }
+    }
+  }
 }
