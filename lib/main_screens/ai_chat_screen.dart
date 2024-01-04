cc44c70a21dcf1609b7bc51f6330eff4cbf586c0
saci zakaria <sacideveloppeur@gmail.com>
2024-01-04 18:37:38 +0100

    SpinKitDoubleBounce()

--- lib/main_screens/ai_chat_screen.dart
+++ lib/main_screens/ai_chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:dakachatgpt_v2/providers/chat_provider.dart';
 import 'package:dakachatgpt_v2/widgets/bottom_chat_field.dart';
 import 'package:dakachatgpt_v2/widgets/chat_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/my_theme_provider.dart';
@@ -30,7 +31,9 @@ class _AIChatScreenState extends State<AIChatScreen> {
             child: ChatList(),
           ),
           //
-          if (context.watch<ChatProvider>().isTyping) ...[],
+          if (context.watch<ChatProvider>().isTyping) ...[
+            SpinKitDoubleBounce()
+          ],
 
           const SizedBox(height: 15.0),
           const BottomChatField(),
