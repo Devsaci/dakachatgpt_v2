84df8d3fc2bc7faf4afcbb98a3ec40b59b318e11
saci zakaria <sacideveloppeur@gmail.com>
2024-01-05 18:57:53 +0100

    required this.senderId,

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 class ChatWidget extends StatelessWidget {
   // const ChatWidget({super.key});
-  const ChatWidget({Key? key, required this.messageData}) : super(key: key);
+  const ChatWidget({
+    Key? key,
+    required this.messageData,
+    required this.senderId,
+  }) : super(key: key);
 
   final dynamic messageData;
+  final dynamic senderId;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
