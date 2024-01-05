e302c9cacce90bce008e8838ffe932d63055dd03
saci zakaria <sacideveloppeur@gmail.com>
2024-01-05 18:51:06 +0100

    class ChatWidget extends StatefulWidget

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ChatWidget extends StatelessWidget {
+class ChatWidget extends StatefulWidget {
   const ChatWidget({super.key});
 
+  @override
+  State<ChatWidget> createState() => _ChatWidgetState();
+}
+
+class _ChatWidgetState extends State<ChatWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
